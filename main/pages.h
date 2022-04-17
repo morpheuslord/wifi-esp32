3a97b3f0567131fb8480820213c1ddcc7c977257
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2022-04-17 14:54:26 +0530

    Add files via upload

---
+++ main/pages.h
@@ -0,0 +1,284 @@
+#define CONFIG_PAGE "<html>\
+<head></head>\
+<meta name='viewport' content='width=device-width, initial-scale=1'>\
+<style>\
+@import 'https://fonts.googleapis.com/css2?family=Goldman&display=swap';\
+@import 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700';\
+body,html {\
+padding:0px;\
+margin:0px;\
+font-family: 'Comfortaa', cursive;\
+font-weight:bold;\
+color:#fff;\
+background:rgb(0, 0, 0);\
+scroll-behavior: smooth;\
+}\
+\
+td {\
+font-weight: 100;\
+min-height: 24px;\
+}\
+\
+td:first-child { \
+text-align: right;\
+min-width: 100px;\
+padding-right: 10px;\
+}\
+\
+#portfolio div h1 {\
+font-size:40px;\
+background-image: linear-gradient(to left, #FF416C 0%, #FF4B2B 100%);\
+-webkit-background-clip:text;\
+background-clip:text;\
+-webkit-text-fill-color: transparent;\
+background-size:500% 500% !important;\
+-webkit-animation: Gradient 20s ease infinite;\
+-moz-animation: Gradient 20s ease infinite;\
+-o-animation: Gradient 20s ease infinite;\
+animation: Gradient 20s ease infinite;\
+font-family: 'Comfortaa', cursive;\
+}\
+\
+h2 {\
+font-size: 1.2em;\
+font-weight: 200;\
+margin-left: 5px;\
+}\
+\
+input {\
+border: 1px solid rgb(196, 196, 196);\
+color: rgb(76, 76, 76);\
+width: 240px;\
+border-radius: 3px;\
+height: 40px;\
+margin: 3px 0px;\
+padding: 0px 14px;\
+}\
+\
+input:focus {\
+border:1px solid black;\
+outline: none !important;\
+box-shadow: 0 0 10px #719ECE;\
+}\
+\
+#config {\
+width:400px; \
+margin:0 auto;\
+}\
+\
+.ok-button {\
+background-color: #0078e7;\
+background:white;\
+cursor:pointer;\
+color:#000;\
+}\
+\
+.red-button {\
+background-color: #e72e00;\
+color: #fff;\
+}\
+</style>\
+<body>\
+<div id='config'>\
+<h1>ESP32 NAT Router Config</h1>\
+<script>\
+if (window.location.search.substr(1) != '')\
+{\
+document.getElementById('config').display = 'none';\
+document.body.innerHTML ='<h1>ESP32 NAT Router Config</h1>The new settings have been sent to the device.<br/>The page will refresh soon automatically...';\
+setTimeout(\"location.href = '/'\",10000);\
+}\
+</script>\
+<h2>AP Settings (the new network)</h2>\
+<form action='' method='GET'>\
+<table>\
+<tr>\
+<td>SSID</td>\
+<td><input type='text' name='ap_ssid' value='%s' placeholder='SSID of the new network'/></td>\
+</tr>\
+<tr>\
+<td>Password</td>\
+<td><input type='text' name='ap_password' value='%s' placeholder='Password of the new network'/></td>\
+</tr>\
+<tr>\
+<td></td>\
+<td><input type='submit' value='Set' class='ok-button'/></td>\
+</tr>\
+</table>\
+<small>\
+<i>Password </i>less than 8 chars = open<br />\
+</small>\
+</form>\
+\
+<h2>STA Settings (uplink WiFi network)</h2>\
+<form action='' method='GET'>\
+<table>\
+<tr>\
+<td>SSID</td>\
+<td><input type='text' name='ssid' value='%s' placeholder='SSID of existing network'/></td>\
+</tr>\
+<tr>\
+<td>Password</td>\
+<td><input type='text' name='password' value='%s' placeholder='Password of existing network'/></td>\
+</tr>\
+<tr>\
+<td></td>\
+<td><input type='submit' value='Connect' class='ok-button'/></td>\
+</tr>\
+\
+</table>\
+</form>\
+\
+<h2>STA Static IP Settings</h2>\
+<form action='' method='GET'>\
+<table>\
+<tr>\
+<td>Static IP</td>\
+<td><input type='text' name='staticip' value='%s'/></td>\
+</tr>\
+<tr>\
+<td>Subnet Mask</td>\
+<td><input type='text' name='subnetmask' value='%s'/></td>\
+</tr>\
+<tr>\
+<td>Gateway</td>\
+<td><input type='text' name='gateway' value='%s'/></td>\
+</tr>\
+<tr>\
+<td></td>\
+<td><input type='submit' value='Connect' class='ok-button'/></td>\
+</tr>\
+\
+</table>\
+<small>\
+<i>Leave it in blank if you want your ESP32 to get an IP using DHCP</i>\
+</small>\
+</form>\
+\
+<h2>Device Management</h2>\
+<form action='' method='GET'>\
+<table>\
+<tr>\
+<td>Device</td>\
+<td><input type='submit' name='reset' value='Reboot' class='red-button'/></td>\
+</tr>\
+</table>\
+</form>\
+</div>\
+</body>\
+</html>\
+"
+
+#define LOCK_PAGE "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n\
+<html>\
+<head></head>\
+<meta name='viewport' content='width=device-width, initial-scale=1'>\
+<style>\
+@import 'https://fonts.googleapis.com/css2?family=Goldman&display=swap';\
+@import 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700';\
+body,html {\
+padding:0px;\
+margin:0px;\
+font-family: 'Comfortaa', cursive;\
+font-weight:bold;\
+color:#fff;\
+background:rgb(0, 0, 0);\
+scroll-behavior: smooth;\
+}\
+\
+td {\
+font-weight: 100;\
+min-height: 24px;\
+}\
+\
+td:first-child { \
+text-align: right;\
+min-width: 100px;\
+padding-right: 10px;\
+}\
+\
+#portfolio div h1 {\
+font-size:40px;\
+background-image: linear-gradient(to left, #FF416C 0%, #FF4B2B 100%);\
+-webkit-background-clip:text;\
+background-clip:text;\
+-webkit-text-fill-color: transparent;\
+background-size:500% 500% !important;\
+-webkit-animation: Gradient 20s ease infinite;\
+-moz-animation: Gradient 20s ease infinite;\
+-o-animation: Gradient 20s ease infinite;\
+animation: Gradient 20s ease infinite;\
+font-family: 'Comfortaa', cursive;\
+}\
+\
+h2 {\
+font-size: 1.2em;\
+font-weight: 200;\
+margin-left: 5px;\
+}\
+\
+input {\
+border: 1px solid rgb(196, 196, 196);\
+color: rgb(76, 76, 76);\
+width: 240px;\
+border-radius: 3px;\
+height: 40px;\
+margin: 3px 0px;\
+padding: 0px 14px;\
+}\
+\
+input:focus {\
+border:1px solid black;\
+outline: none !important;\
+box-shadow: 0 0 10px #719ECE;\
+}\
+\
+#config {\
+width:400px; \
+margin:0 auto;\
+}\
+\
+.ok-button {\
+background-color: #0078e7;\
+background:white;\
+cursor:pointer;\
+color:#000;\
+}\
+\
+.red-button {\
+background-color: #e72e00;\
+color: #fff;\
+}\
+</style>\
+<body>\
+<div id='config'>\
+<h1>ESP32 NAT Router Config</h1>\
+<script>\
+if (window.location.search.substr(1) != '')\
+{\
+document.getElementById('config').display = 'none';\
+document.body.innerHTML ='<h1>ESP32 NAT Router Config</h1>The new settings have been sent to the device.<br/>The page will refresh soon automatically...';\
+setTimeout(\"location.href = '/'\",1000);\
+}\
+</script>\
+<h2>Config Locked</h2>\
+<form autocomplete='off' action='' method='GET'>\
+<table>\
+<tr>\
+<td>Password:</td>\
+<td><input type='password' name='unlock_password'/></td>\
+</tr>\
+<tr>\
+<td></td>\
+<td><input type='submit' value='Unlock' class='red-button'/></td>\
+</tr>\
+\
+</table>\
+<small>\
+<i>Default: STA password to unlock<br />\
+</small>\
+</form>\
+</div>\
+</body>\
+</html>\
+"
